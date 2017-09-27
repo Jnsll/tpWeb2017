6d8a265599d1c67b581c40db436a95c5c31adfc3
June B-Sallou <june.sallou@laposte.net>
2017-09-27 12:07:04 +0200

    model created

--- model.js
+++ model.js
@@ -1,3 +1,30 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+function Drawing(){
+    var formes = new Array();
+}
+
+function Forme(){
+    var couleur;
+    var epaisseur;
+}
+
+
+function Rectangle(){
+    var x_dep;
+    var y_dep;
+    var largeur;
+    var hauteur;
+    Forme.call(this, couleur, epaisseur);
+}
+
+function Line(){
+
+    var x_init;
+    var y_init;
+    var x_final;
+    var y_final;
+    Forme.call(this, couleur, epaisseur);
+}
